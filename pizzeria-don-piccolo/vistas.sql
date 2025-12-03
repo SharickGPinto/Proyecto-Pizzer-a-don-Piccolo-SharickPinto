74de7f673dc2d60acd1f5d9393d7d9504922a587
Pinto <pinto702rodri@gmail.com>
2025-12-03 09:17:22 -0500

    :construction: vistas construction

--- pizzeria-don-piccolo/vistas.sql
+++ pizzeria-don-piccolo/vistas.sql
@@ -1,6 +1,42 @@
 /*Vista de resumen de pedidos por cliente (nombre del 
 cliente, cantidad de pedidos, total gastado).
+
 Vista de desempeño de repartidores 
 (número de entregas, tiempo promedio, zona).
+
 Vista de stock de ingredientes por debajo del
  mínimo permitido.*/
+
+
+ CREATE VIEW nombres_clientes AS
+ select p.nombre, 
+        c.id 
+ from persona as p
+ join cliente as c
+ on c.id = p.id;
+
+
+--
+CREATE VIEW pedidos_cliente AS 
+ select p.nombre,
+        SUM(pe.total) as total_gastado,
+        COUNT(*) as cantidad_pedidos
+ from persona as p 
+ join pedido as pe 
+ on p.id = pe.id_cliente 
+ GROUP BY pe.id_cliente;       
+
+
+SELECT p.nombre,
+       r.id,
+       z.ubicacion,
+       COUNT(*) as numero_entregas
+FROM persona as p 
+JOIN repartidor as r
+on r.id = p.id
+JOIN zona as z
+on r.id_zona = z.id
+JOIN domicilio as d  
+on r.id = d.id_repartidor
+GROUP BY d.id_repartidor;
+
