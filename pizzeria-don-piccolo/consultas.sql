a4be363d92cfbe089b2ff6f53266cc7b5bea9f84
Pinto <pinto702rodri@gmail.com>
2025-12-03 18:18:25 -0500

    :construction: finished views building queries

--- pizzeria-don-piccolo/consultas.sql
+++ pizzeria-don-piccolo/consultas.sql
@@ -9,6 +9,9 @@ Subconsulta para obtener los clientes frecuentes
 (más de 5 pedidos mensuales).*/
 
 
+--Consultas SQL requeridas
+--Clientes con pedidos entre dos fechas (BETWEEN).
+
 SELECT per.nombre as nombre_persona, ped.fecha_hora as fecha_pedido, ped.id as id_pedido  
 from pedido ped left join persona per on ped.id_cliente=per.id 
 where ped.fecha_hora between '2025-01-01'  and '2025-12-31';
@@ -22,5 +25,68 @@ where ped.fecha_hora between fecha_inicio  and fecha_final;
 end; //
 DELIMITER ;
 
+CALL pedidos_cliente_xmes('2025-01-01 00:00:00', '2025-12-31 23:59:59');
 
 
+--Pizzas más vendidas (GROUP BY y COUNT).
+
+SELECT pi.id,
+       pi.nombre,
+       COUNT(*) as pizzas_mas_vendidas
+FROM pizza as pi 
+JOIN pedido_pizza as pe  
+ON pi.id = pe.id_pizza
+GROUP BY 
+         pi.nombre
+ORDER BY 
+    pizzas_mas_vendidas DESC;
+
+DELIMITER // 
+CREATE PROCEDURE pizzas_mas_vendidas()
+begin
+SELECT pi.id,
+       pi.nombre,
+       COUNT(*) as pizzas_mas_vendidas
+FROM pizza as pi 
+JOIN pedido_pizza as pe  
+ON pi.id = pe.id_pizza
+GROUP BY
+         pi.nombre
+ORDER BY 
+    pizzas_mas_vendidas DESC;
+end; //
+DELIMITER ;
+
+call pizzas_mas_vendidas();
+
+
+--Pedidos por repartidor (JOIN).
+
+ select p.id as id_repartidor,
+        p.nombre,
+        do.id as id_domicilio,
+        COUNT(*) as cantidad_pedidos
+ from persona as p 
+ join domicilio as do 
+ on p.id = do.id_repartidor   
+ GROUP BY do.id_repartidor;  
+
+ DELIMITER //
+ CREATE PROCEDURE pedidos_x_repartidor()
+ begin
+  Select p.id as id_repartidor,
+        p.nombre,
+        do.id as id_domicilio,
+        COUNT(*) as cantidad_pedidos
+ from persona as p 
+ join domicilio as do 
+ on p.id = do.id_repartidor   
+ GROUP BY do.id_repartidor;
+ END; //
+ DELIMITER ;
+
+CALL pedidos_x_repartidor();
+
+--Promedio de entrega por zona (AVG y JOIN).
+
+select AVG()
--- pizzeria-don-piccolo/vistas.sql
+++ pizzeria-don-piccolo/vistas.sql
@@ -15,8 +15,9 @@ Vista de stock de ingredientes por debajo del
  join cliente as c
  on c.id = p.id;
 
+--Vista de resumen de pedidos por cliente (nombre del 
+--cliente, cantidad de pedidos, total gastado).
 
---
 CREATE VIEW pedidos_cliente AS 
  select p.nombre,
         SUM(pe.total) as total_gastado,
@@ -24,7 +25,12 @@ CREATE VIEW pedidos_cliente AS
  from persona as p 
  join pedido as pe 
  on p.id = pe.id_cliente 
- GROUP BY pe.id_cliente;       
+ GROUP BY pe.id_cliente;     
+
+ SELECT * FROM pedidos_cliente;
+
+--Vista de desempeño de repartidores 
+--(número de entregas, tiempo promedio, zona).
 
 CREATE VIEW desempeno_repartidor AS 
 SELECT p.nombre,
@@ -39,4 +45,16 @@ on r.id_zona = z.id
 JOIN domicilio as d  
 on r.id = d.id_repartidor
 GROUP BY d.id_repartidor;
+
+SELECT * FROM desempeno_repartidor;
+
+
+--Vista de stock de ingredientes por debajo del
+ --mínimo permitido.*/
+CREATE VIEW stock_minimo AS 
+select i.id, i.nombre, i.stock
+FROM ingrediente as i
+WHERE i.stock > 5; 
+
+SELECT * FROM stock_minimo;
 
